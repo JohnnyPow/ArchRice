e2266529f7104a1ba7c80d6f89fdbfd6d5e12db1
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-03-05 16:08:41 +0100

    Added hidpi.sh
    	new file:   etc/profile.d/hidpi.sh

---
+++ etc/profile.d/hidpi.sh
@@ -0,0 +1,8 @@
+# GDK applications
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
+
+# Qt applications
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export QT_SCREEN_SCALE_FACTORS=2.0
+export XDG_CURRENT_DESKTOP=KDE
