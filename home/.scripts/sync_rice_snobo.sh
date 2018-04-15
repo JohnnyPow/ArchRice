2f1b7657750a180492d884fe77f3246e4018a37c
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-04-15 21:33:32 +0200

    changed snobos sync script

--- home/.scripts/sync_rice_snobo.sh
+++ home/.scripts/sync_rice_snobo.sh
@@ -6,3 +6,6 @@ scripts/sync.sh
 scripts/patch_1080p.sh
 scripts/patch_de.sh
 scripts/patch_snobo.sh
+
+cp ~/.scripts/keyboard.xml.snobo ~/.scripts/keyboard.xml
+cp ~/.scripts/mouse.xml.snobo ~/.scripts/mouse.xml
