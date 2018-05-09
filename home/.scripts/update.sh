1c471df5132886ff4ac706d1fd22abf22252e571
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-05-09 22:55:01 +0200

    changed update script to include warnings and uses yay now

--- home/.scripts/update.sh
+++ home/.scripts/update.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-sudo reflector -p https -l 10 --sort rate --save /etc/pacman.d/mirrorlist --verbose
-aurman -Syu --devel --needed --noedit
+yay -Pw | tail -n 10
+read -r -p "Is it safe to update? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  yay -Syu --devel --needed
+  echo -n "3.."
+  sleep 1
+  echo -ne "\r2.."
+  sleep 1
+  echo -ne "\r1.."
+  sleep 1
+fi
