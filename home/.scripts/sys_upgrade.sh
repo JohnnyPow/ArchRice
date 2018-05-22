afd360354bef78a2635f8e9dedaf9c46cc8dcee0
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-05-23 00:48:27 +0200

    upgrade script doesnt clean build every package anymore

--- home/.scripts/sys_upgrade.sh
+++ home/.scripts/sys_upgrade.sh
@@ -3,7 +3,7 @@ yay -Pw | tail -n 10
 read -r -p "Is it safe to update? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-  yay -Syu --devel --needed --sudoloop --answerupgrade " " --answerclean All --answeredit None
+  yay -Syu --devel --needed --sudoloop --answerupgrade " " --answerclean None --answeredit None
   echo -n "3.."
   sleep 1
   echo -ne "\r2.."
