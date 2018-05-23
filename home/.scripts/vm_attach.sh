3b2c4a59ff1a5f1b4120229007e53640fe83912a
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-05-23 21:24:53 +0200

    fixed a small bug in vm_attach

--- home/.scripts/vm_attach.sh
+++ home/.scripts/vm_attach.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 nc -c 127.0.0.1 1234 << END
+device_del usb_kbd
+device_del usb_mouse
 device_add usb-host,vendorid=0x1038,productid=0x1607,id=usb_kbd
 device_add usb-host,vendorid=0x1e7d,productid=0x2e24,id=usb_mouse
 END
