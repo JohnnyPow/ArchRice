82c0bd484a6ff8b72683a32f0809f425605eb42e
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-06-02 17:11:26 +0200

    added mouse and keyboard ids to src

--- home/.scripts/vm_attach.sh
+++ home/.scripts/vm_attach.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.config/arch_rice_src
+
 if [[ -z $(netstat -tln | grep 127.0.0.1:1234) ]]; then
   echo "VM not running"
   exit
@@ -10,8 +12,8 @@ killall synergy synergyc
 nc -N 127.0.0.1 1234 << END
 device_del usb_kbd
 device_del usb_mouse
-device_add usb-host,vendorid=0x1038,productid=0x1607,id=usb_kbd
-device_add usb-host,vendorid=0x1e7d,productid=0x2e24,id=usb_mouse
+device_add usb-host,"$vm_kbd",id=usb_kbd
+device_add usb-host,"$vm_mouse",id=usb_mouse
 END
 
 synergy &
