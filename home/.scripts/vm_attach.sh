685f9dc9eba7647d724759706fc02d2f8cd2b091
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-06-02 17:13:49 +0200

    fixed a bug in the attach script

--- home/.scripts/vm_attach.sh
+++ home/.scripts/vm_attach.sh
@@ -12,8 +12,8 @@ killall synergy synergyc
 nc -N 127.0.0.1 1234 << END
 device_del usb_kbd
 device_del usb_mouse
-device_add usb-host,"$vm_kbd",id=usb_kbd
-device_add usb-host,"$vm_mouse",id=usb_mouse
+device_add usb-host,$vm_kbd,id=usb_kbd
+device_add usb-host,$vm_mouse,id=usb_mouse
 END
 
 synergy &
