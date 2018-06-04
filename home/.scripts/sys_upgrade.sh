2dfc12d94b80437fb7e4ba4fd8ade5f70aa3d2c2
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-06-04 22:31:54 +0200

    changed update script to skip news if there are none

--- home/.scripts/sys_upgrade.sh
+++ home/.scripts/sys_upgrade.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-yay -Pw | tail -n 10
-read -r -p "Is it safe to update? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
+
+function upgrade {
   yay -Syu --devel --needed --sudoloop --answerupgrade " " --answerclean All --answeredit None
+}
+
+news=$(yay -Pw)
+if [[ -n "$(echo $news)" ]]; then
+  echo $news
+  read -r -p "Is it safe to update? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    upgrade
+  fi
+else
+  upgrade
 fi
 $SHELL
