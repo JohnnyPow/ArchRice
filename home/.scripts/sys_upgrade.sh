731fc9a387331d66a7a10b39578bf5df055841a9
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-06-04 22:04:31 +0200

    upgrade script keeps the shell open after completion

--- home/.scripts/sys_upgrade.sh
+++ home/.scripts/sys_upgrade.sh
@@ -4,10 +4,5 @@ read -r -p "Is it safe to update? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
   yay -Syu --devel --needed --sudoloop --answerupgrade " " --answerclean All --answeredit None
-  echo -n "3.."
-  sleep 1
-  echo -ne "\r2.."
-  sleep 1
-  echo -ne "\r1.."
-  sleep 1
 fi
+$SHELL
