6f9a2db11fd9dfd6fac3e80242dcc512d3ec3de5
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-08-27 15:19:51 +0200

    or not :D

--- misc/rice_upgrade.sh
+++ misc/rice_upgrade.sh
@@ -3,6 +3,6 @@
 cd ~/.rice
 git pull origin master
 git submodule update
-git submodule foreach misc/git_pristine.sh
+git submodule foreach ~/.rice/misc/git_pristine.sh
 git reset --hard
 git clean -df
