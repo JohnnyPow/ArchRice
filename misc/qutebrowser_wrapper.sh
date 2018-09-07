7222e5524cca57a4ae71d6d05158d44db20ecbbf
JohnnyPow <32849404+JohnnyPow@users.noreply.github.com>
2018-09-07 14:21:05 +0200

    (default/desktop) added qutebrowser sync

---
+++ misc/qutebrowser_wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd ~/.local/share/qutebrowser/userscripts
+git pull origin master
+git reset --hard
+git clean -dfxn
+cd ~/.config/qutebrowser
+git pull origin master
+git reset --hard
+git clean -dfxn
+case $1 in
+  private)
+    qutebrowser ":open -p" --nowindow
+  ;;
+  *)
+    qutebrowser
+  ;;
+esac
+git add -A
+git commit -a -m "script update"
+git push
